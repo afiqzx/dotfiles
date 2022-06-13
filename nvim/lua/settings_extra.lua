28730baf3cddb3eea7a76c82a0ed4ce8389f12b4
Mohammad Afiq <mohammadafiqrozelan@gmail.com>
2022-06-13 23:29:36 +0900

    fix gruvbox setting

--- nvim/lua/settings_extra.lua
+++ nvim/lua/settings_extra.lua
@@ -1,2 +1,19 @@
+local global = vim.o
+local kmap = vim.api.nvim_set_keymap
 
-vim.cmd'colorscheme gruvbox'
+--local utils = require('gruvbox')
+
+-- I don't know how to translate this to lua
+vim.cmd'let g:gruvbox_number_column = \'bg1\''
+vim.cmd'let g:gruvbox_underline = \'1\''
+
+vim.cmd'autocmd vimenter * ++nested colorscheme gruvbox'
+
+
+--options = { noremap = true, silent = true }
+--options = { noremap = true }          -- this is for debugging purpose (not silent)
+-- Switching buffers keymap (do I need this actually???)
+--kmap('n', '<left>', ':bp<CR>', options)
+
+
+
