0ca00bc8e91b7bb71cb0779461253264a3b8bf0a
Mohammad Afiq <mohammadafiqrozelan@gmail.com>
2022-06-13 21:39:54 +0900

    start exporting to lua

---
+++ nvim/lua/settings.lua
@@ -0,0 +1,18 @@
+local global = vim.o
+local kmap = vim.api.nvim_set_keymap
+
+-- use hybrid line numbering
+global.relativenumber = true
+global.number = true
+
+-- insert space in tab
+global.expandtab = true
+global.shiftwidth = 4
+
+options = { noremap = true, silent = true }
+--options = { noremap = true }          -- this is for debugging purpose (not silent)
+-- Switching buffers keymap (do I need this actually???)
+kmap('n', '<left>', ':bp<CR>', options)
+kmap('n', '<right>', ':bn<CR>', options)
+
+
