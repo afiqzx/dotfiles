e079cb7eb8714ffa547d8b8b051fcfe1b4e8b26e
Mohammad Afiq <46773936+afiqzx@users.noreply.github.com>
2023-03-19 18:43:47 +0900

    Update deprecated nvim-lsp stuff
    Signed-off-by: Mohammad Afiq <46773936+afiqzx@users.noreply.github.com>

--- nvim/lua/settings_lsp.lua
+++ nvim/lua/settings_lsp.lua
@@ -72,7 +72,7 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', 'gr'              , '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   buf_set_keymap('n', '<space>e'        , '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
   buf_set_keymap('n', '<space>q'        , '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-  buf_set_keymap('n', '<space>f'        , "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  buf_set_keymap('n', '<space>f'        , "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", opts)
 
 end
 
